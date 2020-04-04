c11243231749dd87c95a69a6c2af8e14ca303bcc
sheapa <55968932+sheapa@users.noreply.github.com>
2020-04-03 23:27:25 -0700

    Created on click even for devoured button in public burger.js. Changed container to fluid in index.handlebars.

---
+++ public/assets/js/burger.js
@@ -0,0 +1,22 @@
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+        var id = $(this).data("id");
+        var newDevoured = $(this).data("newsleep");
+    
+        var newDevouredState = {
+            devoured: newDevoured
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burger/" + id, {
+            type: "PUT",
+            data: newDevoured
+        }).then(
+            function() {
+            console.log("changed sleep to", newDevoured);
+            // Reload the page to get the updated list
+            location.reload();
+            }
+        );
+    });
+});
